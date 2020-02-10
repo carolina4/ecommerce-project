af25415fa30899522a9b12d390adcf1d2bee57aa
Carolina Valerio <carolina.saenz@hired.com>
2020-02-10 13:52:40 -0600

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './containers/homepage.component';
 
+const HatsPsge = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+);
+
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/hats' component={HatsPsge} />
+      </Switch>
     </div>
   );
 }
